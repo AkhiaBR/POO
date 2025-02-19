9590078d461269154f1c61ded37d39e6e4c949f3
Fernando <fernandooogoncalves@gmail.com>
2025-02-19 15:22:00 -0300

    first commit

---
+++ exemplos/src/MediaAlunos.java
@@ -0,0 +1,16 @@
+public class MediaAlunos
+{
+    public static void main(String[] args)
+    {
+        double n1, n2, n3, n4, media;
+
+        n1 = 6;
+        n2 = 7;
+        n3 = 5.5;
+        n4 = 8;
+
+        media = (n1+n2+n3+n4)/4;
+
+        System.out.println("Média das notas dos alunos: "+media);
+    }
+}
---
+++ exemplos/src/Temperatura.java
@@ -0,0 +1,27 @@
+// exemplo 01 sobre
+
+public class Temperatura // classe pública com nome: Temperatura
+{
+    /* void (método não possui retorno)
+       estático (método pertence a classe)
+       main(String[] args) - array de strings (String[]) e args pois sera recebido um INPUT
+     */
+    public static void main(String[] args)
+    {
+        double celsius, fah, kelvin, re, ra; // criação de variáveis decimais
+
+        celsius = 30; // valor da temperatura inicial
+
+        // fórmulas:
+        fah = celsius * 1.8 + 32;
+        kelvin = celsius + 273.15;
+        re = celsius * 0.8;
+        ra = celsius * 1.8 + 32 + 459.67;
+
+        // print das temperaturas convertidas
+        System.out.println("Temperatura em Fahrenheit: "+fah);
+        System.out.println("Temperatura em Kelvin: "+kelvin);
+        System.out.println("Réaumur: "+re);
+        System.out.println("Rankine: "+ra);
+    }
+}
