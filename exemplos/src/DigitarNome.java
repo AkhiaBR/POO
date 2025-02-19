0c1f758d78ce48ab86f8fbaf536d6c63ecfa572e
Fernando <fernandooogoncalves@gmail.com>
2025-02-19 16:11:29 -0300

    add: DigitarNome.java

---
+++ exemplos/src/DigitarNome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DigitarNome
+{
+    public static void main(String[] args)
+    {
+        int idadeUsuario = 20;
+        String nomeUsuario = "João";
+
+        System.out.print("A pessoa tem o nome "+nomeUsuario);
+        System.out.print(", ");
+        System.out.println("e ela tem "+idadeUsuario+" anos");
+    }
+}
